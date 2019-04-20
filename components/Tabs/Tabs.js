69932615e4c96507cf44d8d03b7aadb041b0efb5
Gilbert Olsen <gilbertolsen@Gilberts-MacBook-Pro.local>
2019-04-20 11:52:09 -0400

    saving before close

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -9,18 +9,21 @@ class TabLink {
     // Using the custom data attribute, get the associated Item element
     //this.itemElement= document.querySelector(".tabs-item");
     this.itemElement = document.querySelector(
-      `.tabs-item[data-tab="${this.data}"]`
+      `.tabs-item[data-tab="${this.data}"]` //selecting the tabs-item the bracket notation !!!!!!
+      //filters anything with that class AND the custom data attribute
     );
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
     // Add a click event listener on this instance (of the constructor!!!!?!), calling the select method on click
 
-    this.element.addEventLisener("click", () => this.select()); // select is called on THIS instance of the constructor
+    //this.element.addEventLisener("click", () => this.select()); // select is called on THIS instance of the constructor
+    //constructor is baking a brownie with that feature
   }
 
   select() {
     // Get all of the elements with the tabs-link class
-    //const links;
+    const links = document.querySelectorAll(".tabs-link");
+    console.log(links);
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     //Array.from(links).forEach();
     // Add a class named "tabs-link-selected" to this link
@@ -60,7 +63,7 @@ links = document
   .querySelectorAll(".tabs-link")
   .forEach(eachLinkTab => new TabLink(eachLinkTab));
 
-console.log(links);
+//console.log(links);
 
 //    es5 function Version ????
 //    forEach(function(eachLinkTab) {
